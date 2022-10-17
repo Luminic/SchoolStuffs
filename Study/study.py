75ff2cc2beed316231d4f9cbf564f180681aa1d8
lux <lukas.dot.py@gmail.com>
2022-10-17 01:19:51 -0700

    update study

--- Study/study.py
+++ Study/study.py
@@ -1,7 +1,9 @@
 import pathlib
 import csv
-from typing import Optional
+import traceback
+from typing import Iterable, Optional
 import random
+
 
 def select_file_dialog() -> Optional[pathlib.Path]:
     current_dir = pathlib.Path(__file__).parent.resolve()
@@ -112,79 +114,187 @@ def parse_input(inp: str) -> str:
 def scoring(correct: int, incorrect: int) -> int:
     return correct - incorrect * 2
 
+class StudyItem:
+    def __init__(self, data: list[str] | list[str | int]):
+        """
+        `data` should be a line of csv input
+        """
+
+        assert(len(data) == 4)
+
+        self._data = data
+        for i in (2,3,):
+            if type(self._data[i] == str):
+                self._data[i] = int(self._data[i])
+    
+    def __repr__(self) -> str:
+        return f"StudyItem({self._data})"
+    
+    @property
+    def term(self) -> str:
+        return self._data[0]
+    
+    @property
+    def definition(self) -> str:
+        return self._data[1]
+    
+    @property
+    def correct(self) -> int:
+        return self._data[2]
+    
+    @correct.setter
+    def correct(self, newval: int):
+        self._data[2] = newval
+
+    @property
+    def incorrect(self) -> int:
+        return self._data[3]
+    
+    @correct.setter
+    def incorrect(self, newval: int):
+        self._data[3] = newval
+
+    @property
+    def score(self) -> int:
+        return self.correct - self.incorrect * 2
+
+
+class StudySet:
+    def __init__(self, data: Optional[pathlib.Path | list[list[str]]] = None):
+        """
+        `data` should be the path to a file or a csv in the form of a list of lists
+
+        Manually using `load` and `save` is spported but not recommended; use `with` statements instead
+        """
+
+        self._path = None
+        self._study_set: list[StudyItem] = []
+
+        if isinstance(data, pathlib.Path):
+            self._path = data
+        elif isinstance(data, Iterable):
+            for line in data:
+                self._study_set.append(StudyItem(line))
+
+    def load(self, path: Optional[str] = None):
+        if path is not None:
+            self._path = path
+
+        with open(self._path, 'r') as f:
+            for line in csv.reader(f):
+                self._study_set.append(StudyItem(line))
+    
+    def __enter__(self):
+        self.load()
+        return self
+    
+    def save(self, path: Optional[pathlib.Path] = None):
+        if path is not None:
+            self._path = path
+            
+        with open(self._path, 'w') as f:
+            writer = csv.writer(f)
+            for item in self._study_set:
+                writer.writerow(item._data)
+        
+    def __exit__(self, exc_type, exc_value, tb):
+        self.save()
+
+        if exc_type is not None:
+            traceback.print_exception(exc_type, exc_value, tb)
+            return False
+        return True
+        
+    def __repr__(self) -> str:
+        return f"StudySet({self._study_set})"
+
+    def get_batch(self, batch_size: int) -> list[StudyItem]:
+        batch = []
+        if len(self._study_set) <= batch_size:
+            batch = self._study_set
+        else:
+            sorted_study_set = sorted(self._study_set, key = lambda item: item.score)
+            current_score = None
+            current_score_index = None
+            for i, item in enumerate(sorted_study_set):
+                if len(batch) < batch_size:
+                    batch.append(item)
+                    if current_score is None or current_score < item.score:
+                        current_score = item.score
+                        current_score_index = i
+                elif item.score == current_score:
+                    batch.append(item)
+                else:
+                    break
+
+            if len(batch) > batch_size:
+                extras = batch[current_score_index:]
+                batch = batch[:current_score_index]
+                random.shuffle(extras)
+                batch += extras[:(batch_size - len(batch))]
+
+        random.shuffle(batch)
+        return batch
+
+    def get_overall_scores(self) -> tuple[int, int]:
+        best = None
+        worst = None
+
+        for item in self._study_set:
+            if best is None or item.score > best:
+                best = item.score
+            if worst is None or item.score < worst:
+                worst = item.score
+
+        return (best, worst)
 
 def study(file: pathlib.Path):
     batch_size = 7
 
-    study_set = []
-    with open(file, 'r') as f:
-        for line in csv.reader(f):
-            line[2] = int(line[2])
-            line[3] = int(line[3])
-            study_set.append(line)
+    with StudySet(file) as study_set:
+        sets = 0
+        while 1:
+            sets += 1
+            current_batch = study_set.get_batch(batch_size)
 
-    sets = 0
-    while 1:
-        sets += 1
+            print(f"\n~=~=~ Set {sets} ~=~=~")
 
-        current_batch = []
-        if len(study_set) <= batch_size:
-            current_batch = study_set
-        else:
-            sorted_study_set = sorted(study_set, key = lambda item: scoring(item[2],item[3]))
-            current_score = -1
-            for item in sorted_study_set:
-                if len(current_batch) < batch_size:
-                    current_batch.append(item)
-                    current_score = scoring(item[2], item[3])
-                elif scoring(item[2], item[3]) == current_score:
-                    current_batch.append(item)
-                else:
-                    break
+            try:
+                for item in current_batch:
+                    print(item.term)
 
-            random.shuffle(current_batch)
-            current_batch = current_batch[:batch_size]
+                    inp = parse_input(input(": "))
+                    correct = (set(inp.split(", ")) == set(item.definition.split(", ")))
 
-        print(f"~=~=~ Set {sets} ~=~=~")
+                    if correct:
+                        message = "Correct"
+                        item.correct += 1
+                    else:
+                        message = "Incorrect"
+                        item.incorrect += 1
 
-        try:
-            for item in current_batch:
-                print(item[0])
-
-                inp = parse_input(input(": "))
-                correct = (set(inp.split(", ")) == set(item[1].split(", ")))
-
-                if correct:
-                    message = "Correct"
-                    item[2] += 1
-                else:
-                    message = "Incorrect"
-                    item[3] += 1
-
-                inp = input(f"{message}: {item[1]} ")
-                print()
-                if inp == 'i':
-                    correct = False
-                    print("Overridden to incorrect")
-                    item[2] -= 1
-                    item[3] += 1
-                elif inp == 'c':
-                    correct = True
-                    print("Overridden to correct")
-                    item[2] += 1
-                    item[3] -= 1
-                
-        except KeyboardInterrupt:
-            print("\nTerminated batch early")
-        
-        print(current_batch)
-        inp = input("Continue? ")
-        if inp in ('q', "quit", 'n', "no"):
-            break
-    
-    with open(file, 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(study_set)
+                    inp = input(f"{message}: {item.definition} ")
+                    print()
+                    if inp == 'i':
+                        correct = False
+                        print("Overridden to incorrect")
+                        item.correct -= 1
+                        item.incorrect += 1
+                    elif inp == 'c':
+                        correct = True
+                        print("Overridden to correct")
+                        item.correct += 1
+                        item.incorrect -= 1
+                    
+            except KeyboardInterrupt:
+                print("\nTerminated batch early")
+            
+            print(current_batch)
+            scores = study_set.get_overall_scores()
+            print(f"Current scores: best: {scores[0]} worst: {scores[1]}")
+            inp = input("Continue? ")
+            if inp in ('q', "quit", 'n', "no"):
+                break
     
     print(f"Data saved to {file.absolute()}")
 
